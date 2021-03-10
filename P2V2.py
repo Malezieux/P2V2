47a8c56fff69b828e27c688d78a6a6ed2f0b31a0
Malezieux <philippemalezieux@gmail.com>
2021-03-10 11:59:39 +0100

    version image + csv

--- P2V2.py
+++ P2V2.py
@@ -9,40 +9,21 @@ import re
 import csv
 from csv import DictWriter
 import pathlib
+import module
 
+#liste de toute les pages
 
+numero=3
 page = requests.get(
-    'http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html'
+    module.page_catalogue(numero)
 )
 if page.status_code == requests.codes.ok:
 
     page = BeautifulSoup(page.content, 'html.parser')
-    product_page_url = 'test'
-    title = page.find('h1').text
-    upc = page.find('th', text='UPC').find_next('td').text
-    price_including_tax = (
-        page.find('th', text='Price (incl. tax)').find_next('td').text
-    )
-    price_excluding_tax = (
-        page.find('th', text='Price (excl. tax)').find_next('td').text
-    )
-    number_available_before = (
-        page.find('th', text='Availability').find_next('td').text
-    )
-    number_available = int(re.search('\d+', number_available_before).group(0))
-    review_rating = (
-        page.find('th', text='Number of reviews').find_next('td').text
-    )
-    product_description = page.find('h2').find_next('p').text
-    category = page.find_all('li')[1].text
-    image_url = page.find('img')['src']
-    f = (title, upc, price_including_tax, price_excluding_tax)
-    print(f)
-with open('test.csv', 'w', newline='') as csvfile:
 
-    list_item = csv.writer(csvfile, delimiter=' ', quotechar='|')
-    list_item.writerows(
-        ('title', 'upc', 'price_including_tax', 'price_excluding_tax')
-    )
+for div in page.select('h3 a'):
+    list_url= (div.get('href'))[6:]
+    print(list_url)
 
-    list_item.writerows((f))
+print (module.page_catalogue(25))
+
--- module.py
+++ module.py
@@ -2,13 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 from csv import DictWriter
 
-#url= 'https://books.toscrape.com/catalogue/sharp-objects_997/index.html'
-def final_csv(mydata):
+url= 'https://books.toscrape.com/catalogue/sharp-objects_997/index.html'
+
+
+
+
+def page_catalogue(my_list):
+    page_num = 'page-' + my_list + '.html'
+    my_url = 'https://books.toscrape.com/catalogue/category/books_1/'+ page_num
+    print (my_url)
+
+
+
+def final_csv(data):
     with open('testh.csv', 'w', newline='', encoding='UTF-8') as csvfile:
         fieldnames=['product_page_url','title', 'upc', 'price_including_tax', 'price_excluding_tax', 'number_available', 'review_rating', 'product_description', 'category', 'image_url']
         list_item = DictWriter(csvfile, delimiter='|', fieldnames=fieldnames, quotechar='|')
     #for item in data:
         list_item.writeheader()
     
-        list_item.writerow(mydata)
-        print(mydata)
+        list_item.writerow(data)
+        print(data)
