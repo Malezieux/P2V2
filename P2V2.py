0150e638419f74ebba0ed1789f7d0d25143599d9
malezieux philippe <PHM@afnor.fr>
2021-03-25 09:42:24 +0100

    version url by categorie

--- P2V2.py
+++ P2V2.py
@@ -1,14 +1,18 @@
 import module
 
 __url__ = 'https://books.toscrape.com/catalogue/category/books_1/index.html'
-#i ='https://books.toscrape.com/catalogue/
-# scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html'
 
-final_list_url = [module.list_url(__url__)]
+  
+final_list_url = module.list_url(__url__)
 listsui = module.next_url(__url__)
 # i est un item de list ?
+books_info_list = []
 for i in final_list_url:
-    module.list_item(i)
-print (final_list_url)
-
-print(i)
+    #print (i)
+    books_info_list.append(module.list_item(i))
+  
+module.final_csv(books_info_list)
+    
+#print (final_list_url)
+print (listsui)
+#print(i)
--- module.py
+++ module.py
@@ -13,9 +13,10 @@ def list_url(url):
         )
     if page.status_code == requests.codes.ok:
         page = BeautifulSoup(page.content, 'html.parser')
+    list_url_page = []
     for div in page.select('h3 a'):
-        list_url_page = 'https://books.toscrape.com/catalogue/'+(div.get('href'))[6:]
-        return (list_url_page)
+        list_url_page.append('https://books.toscrape.com/catalogue/'+(div.get('href'))[6:])
+    return list_url_page
 
 # select url neext page for liste_url
 def next_url(url):
@@ -25,14 +26,14 @@ def next_url(url):
     if page.status_code == requests.codes.ok:
             page = BeautifulSoup(page.content, 'html.parser')
     url_suite = url[0:-10] + page.find('a', text='next').get('href')
-    return(url_suite)
+    return url_suite
 
 # select all item by book
 def list_item (i):
 
-    page = requests.get(i)
-    if page.status_code == requests.codes.ok:
-        page = BeautifulSoup(page.content, 'html.parser')
+        page = requests.get(i)
+        if page.status_code == requests.codes.ok:
+            page = BeautifulSoup(page.content, 'html.parser')
 
 
         product_page_url = i
@@ -61,18 +62,18 @@ def list_item (i):
         data = {'product_page_url':product_page_url,'title':title,'upc':upc,'price_including_tax':price_including_tax,'price_excluding_tax':price_excluding_tax,'number_available':number_available,'review_rating':review_rating,'product_description':product_description,'category':category,'image_url':image_url}
         
         # fonction final_csv create csv 
-        final_csv(data)
-        
+        return data
 
 
 
-
+#export csv
 def final_csv(data):
     with open('test.csv', 'w', newline='', encoding='UTF-8') as csvfile:
         fieldnames=['product_page_url','title', 'upc', 'price_including_tax', 'price_excluding_tax', 'number_available', 'review_rating', 'product_description', 'category', 'image_url']
-        list_item = DictWriter(csvfile, delimiter='|', fieldnames=fieldnames, quotechar='|')
+        list_item_csv = DictWriter(csvfile, delimiter='|', fieldnames=fieldnames, quotechar='|')
     #for item in data:
-        list_item.writeheader()
+        list_item_csv.writeheader()
     
-        list_item.writerow(data)
+        for item in data:
+            list_item_csv.writerow (item)
         print(data)
