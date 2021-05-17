b2187547b25ecd74f600fbc9de77a04aa2de6310
malezieux philippe <PHM@AFNOR.FR>
2021-05-17 15:39:49 +0200

    all url book

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #! /usr/bin/venv python3
 # coding: utf-8
-from module import *
+import requests
+from bs4 import BeautifulSoup
 
 
 url_first = 'https://books.toscrape.com/'
-
 page = requests.get(url_first)
 if page.status_code == requests.codes.ok:
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -12,29 +12,30 @@ if page.status_code == requests.codes.ok:
     category_urls = []
     for _ in side_categories.find_all('a')[1:]:
         category_urls.append('https://books.toscrape.com/' + _.get('href'))
-    category_urls_next = []
-    for _ in category_urls:
-        page = requests.get(_)
-        if page.status_code == requests.codes.ok:
-            soup = BeautifulSoup(page.content, 'html.parser')
-            if soup.find('a', text='next'):
-                category_urls_next.append(
-                    _.replace('index.html', 'page-2.html')
-                )
-    resturl = []
-    for _ in category_urls_next:
-        i = [3, 4, 5, 6, 7]
-        urlnext = _[:-11]
 
+    category_next_urls = []
+    list_book_page = []
+    for _ in category_urls:
+        url = _[:-10]
+        i = [1, 2, 3, 4, 5, 6, 7, 8]
         for _ in i:
-            urlz = f'{urlnext}page-{_}.html'
-            page = requests.get(urlz)
+            urlnext = f'{url}page-{_}.html'
+            page = requests.get(urlnext)
             if page.status_code == requests.codes.ok:
-                resturl.append(urlz)
+                category_next_urls.append(urlnext)
+                soup = BeautifulSoup(page.content, 'html.parser')
+                for div in soup.select('h3 a'):
+                    list_book_page.append(
+                        'https://books.toscrape.com/catalogue/'
+                        + (div.get('href')[9:])
+                    )
+
             else:
                 pass
-category_urls.extend(category_urls_next + resturl)
-print(category_urls)
 
+
+category_urls.extend(category_next_urls)
+print(category_urls)
+print(list_book_page)
 
 # print(url_book)
