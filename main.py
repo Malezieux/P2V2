85cde58a033220e92850cf28932656373cdc81e1
malezieux philippe <PHM@AFNOR.FR>
2021-05-26 08:22:33 +0200

    ligne 15 de module.py pour gestion volume

--- main.py
+++ main.py
@@ -1,51 +1,17 @@
 #! /usr/bin/venv python3
 # coding: utf-8
+
 import module
-import requests
-from bs4 import BeautifulSoup
+import os
 
-
+os.makedirs('images')
 url_first = 'https://books.toscrape.com/'
-page = requests.get(url_first)
-if page.status_code == requests.codes.ok:
-    soup = BeautifulSoup(page.content, 'html.parser')
-    side_categories = soup.find('div', class_='side_categories')
-    category_urls = []
-    for _ in side_categories.find_all('a')[1:]:
-        category_urls.append('https://books.toscrape.com/' + _.get('href'))
+category_urls = module.category_url(url_first)
 
-    category_next_urls = []
-    list_book_page = []
-    for _ in category_urls:
-        page = requests.get(_)
-        if page.status_code == requests.codes.ok:
-            category_next_urls.append(_)
-            soup = BeautifulSoup(page.content, 'html.parser')
-            for div in soup.select('h3 a'):
-                list_book_page.append(
-                    'https://books.toscrape.com/catalogue/'
-                    + (div.get('href')[9:])
-                )
-    for _ in category_urls:
-        url = _[:-10]
-        i = [2, 3, 4, 5, 6, 7, 8]
-        for _ in i:
-            urlnext = f'{url}page-{_}.html'
-            page = requests.get(urlnext)
-            if page.status_code == requests.codes.ok:
-                category_next_urls.append(urlnext)
-                soup = BeautifulSoup(page.content, 'html.parser')
-                for div in soup.select('h3 a'):
-                    list_book_page.append(
-                        'https://books.toscrape.com/catalogue/'
-                        + (div.get('href')[9:])
-                    )
-            else:
-                pass
+list_book_page = module.category_urls_next(category_urls)
+
+
 item_book = []
 for _ in list_book_page:
     item_book.append(module.list_item_by_book(_))
     module.final_csv(item_book)
-print(item_book)
-
-# print(url_book)
--- module.py
+++ module.py
@@ -1,11 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 import urllib.request
 from csv import DictWriter
 from bs4 import BeautifulSoup
-import pylint
+import re
 
+# select all category
+def category_url(url_first):
+    page = requests.get(url_first)
+    if page.status_code == requests.codes.ok:
+        soup = BeautifulSoup(page.content, 'html.parser')
+        side_categories = soup.find('div', class_='side_categories')
+        category_urls = []
+        for _ in side_categories.find_all('a')[1:2]:
+            category_urls.append('https://books.toscrape.com/' + _.get('href'))
+        return category_urls
+    print(category_urls)
+
+
+# select all url book by category
+def category_urls_next(category_urls):
+    category_next_urls = []
+    list_book_page = []
+    for _ in category_urls:
+        page = requests.get(_)
+        if page.status_code == requests.codes.ok:
+            category_next_urls.append(_)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            for div in soup.select('h3 a'):
+                list_book_page.append(
+                    'https://books.toscrape.com/catalogue/'
+                    + (div.get('href')[9:])
+                )
+        url = _[:-10]
+        next_page = True
+        last_page = 1
+        while next_page:
+            last_page += 1
+            urlnext = f'{url}page-{last_page}.html'
+            try:
+                page = requests.get(urlnext)
+                page.raise_for_status()
+            except requests.HTTPError:
+                next_page = False
+            else:
+                if page.status_code == requests.codes.ok:
+                    category_next_urls.append(urlnext)
+                    soup = BeautifulSoup(page.content, 'html.parser')
+                    for div in soup.select('h3 a'):
+                        list_book_page.append(
+                            'https://books.toscrape.com/catalogue/'
+                            + (div.get('href')[9:])
+                        )
+                else:
+                    pass
+    return list_book_page
 
 
 # select all item by book
@@ -26,16 +75,19 @@ def list_item_by_book(url):
         number_available_before = (
             page.find('th', text='Availability').find_next('td').text
         )
-        number_available = int(re.search('\d+', number_available_before).group(0))
+        number_available = int(
+            re.search('\d+', number_available_before).group(0)
+        )
         review_rating = (
             page.find('th', text='Number of reviews').find_next('td').text
         )
         product_description = page.find('h2').find_next('p').text
         category = page.find_all('li')[2].text[1:-1]
-        image_url = 'images/' + upc + '.jpg'
-        image_url_source = page.find('img')['src']
-        rep_image = 'C:/Travail/03 Formations/P2V2/images/' + upc + '.jpg'
-        image = urllib.request.urlretrieve('https://books.toscrape.com/' + image_url_source[6:], rep_image)
+        image_url = page.find('img')['src']
+        rep_image = 'images/' + upc + '.jpg'
+        image = urllib.request.urlretrieve(
+            'https://books.toscrape.com/' + image_url[6:], rep_image
+        )
         data = {
             'product_page_url': product_page_url,
             'title': title,
@@ -46,14 +98,12 @@ def list_item_by_book(url):
             'review_rating': review_rating,
             'product_description': product_description,
             'category': category,
-            'image_url': image_url,
+            'url_image': rep_image,
         }
-
-        # fonction final_csv create csv
         return data
 
 
-    # export csv
+# export csv
 def final_csv(data):
     with open('test.csv', 'w', newline='', encoding='UTF-8') as csvfile:
         fieldnames = [
@@ -66,14 +116,14 @@ def final_csv(data):
             'review_rating',
             'product_description',
             'category',
-            'image_url',
+            'url_image',
         ]
         list_item_csv = DictWriter(
             csvfile,
             delimiter='|',
             fieldnames=fieldnames,
         )
-    # for item in data:
+        # for item in data:
         list_item_csv.writeheader()
         for _ in data:
             list_item_csv.writerow(_)
