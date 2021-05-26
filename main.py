bb1f2af9222d1482641c0996e85d6ca36c1326e4
malezieux philippe <PHM@AFNOR.FR>
2021-05-26 10:37:52 +0200

    Livrable v1.1

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ list_book_page = booktoscrap.category_urls_next(category_urls)
 item_book = []
 for _ in list_book_page:
     item_book.append(booktoscrap.list_item_by_book(_))
-    booktoscrap.final_csv(item_book)
+
+booktoscrap.final_csv(item_book)
